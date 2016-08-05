52603ac40e115599e9cf47ce18978a8ea3c7dde6
Ricardo Dozzo <Ricardo.Dozzo@adazzle.local>
2016-08-05 13:56:06 +0100

    Diagnostic sample extended

--- IoCComparison.SimpleInjector.DiagnosticSample/Program.cs
+++ IoCComparison.SimpleInjector.DiagnosticSample/Program.cs
@@ -1,4 +1,6 @@
-﻿using IoCComparison.Core;
+﻿using System;
+using System.Linq;
+using IoCComparison.Core;
 using IoCComparison.SimpleInjector.DiagnosticSample.IoC;
 using SimpleInjector;
 using SimpleInjector.Diagnostics;
@@ -7,6 +9,10 @@ namespace IoCComparison.SimpleInjector.DiagnosticSample {
    class Program {
       static void Main(string[] args) {
 
+         DisposableTypeRegisteredAsTransient();
+      }
+
+      private static void DisposableTypeRegisteredAsTransient() {
 
          var container = new Container();
          // resolve primitive and string parameters via appSettings using Typename.paramName as the key
@@ -19,7 +25,10 @@ namespace IoCComparison.SimpleInjector.DiagnosticSample {
          container.Verify(VerificationOption.VerifyOnly);
 
          var result = Analyzer.Analyze(container);
-
+         result
+            .ToList()
+            .ForEach(r => Console.WriteLine("{0}\t{1}\t{2}", r.Severity, r.DiagnosticType, r.Description));
+         
          container.Dispose();
       }
    }
