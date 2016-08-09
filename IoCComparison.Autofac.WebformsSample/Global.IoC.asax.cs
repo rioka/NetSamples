4d1ee703d2cfbc067ed6e8b21352847f57c4a38b
Ricardo Dozzo <Ricardo.Dozzo@adazzle.local>
2016-08-09 17:47:47 +0100

    Refactoring Global.asax to allow for better separation between standard Asp.net code and IoC code

---
+++ IoCComparison.Autofac.WebformsSample/Global.IoC.asax.cs
@@ -0,0 +1,45 @@
+﻿using System.Web;
+using Autofac;
+using Autofac.Integration.Web;
+using IoCComparison.Core;
+
+namespace IoCComparison.Autofac.WebformsSample {
+
+   public partial class Global : HttpApplication, IContainerProviderAccessor {
+
+      // Provider that holds the application container.
+      static IContainerProvider _containerProvider;
+
+      #region IContainerProviderAccessor
+
+      // Instance property that will be used by Autofac HttpModules
+      // to resolve and inject dependencies.
+      public IContainerProvider ContainerProvider {
+         get { return _containerProvider; }
+      }
+
+      #endregion
+
+      #region Internals
+
+      private void SetupContainer() {
+         // Build up your application container and register your dependencies.
+         var builder = new ContainerBuilder();
+
+         builder
+            .RegisterType<DebugLogger>()
+            .AsImplementedInterfaces()
+            .SingleInstance();
+
+         builder
+            .RegisterType<SampleService>()
+            .As<IService>()
+            .PropertiesAutowired()                    // populate Logger property on SampleService
+            .InstancePerRequest();
+
+         _containerProvider = new ContainerProvider(builder.Build());
+      }
+
+      #endregion
+   }
+}
