e531a9101cf43d2ab61a43cf216edc4f20d8b5d3
Ricardo Dozzo <Ricardo.Dozzo@adazzle.local>
2016-11-25 17:18:33 +0000

    New samples with closed generic types

---
+++ AutofacSamples/AutofacSamples.Scenarios/CloseGenericNeedNoSpecialCare.cs
@@ -0,0 +1,97 @@
+﻿using System.Linq;
+using System.Reflection;
+using Autofac;
+using AutofacSamples.Scenarios.Core.ClosedGenerics;
+using AutofacSamples.Scenarios.Core.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutofacSamples.Scenarios {
+
+   [TestClass]
+   public class CloseGenericNeedNoSpecialCare {
+
+      #region Vars
+
+      private ContainerBuilder _builder;
+      private IContainer _container;
+
+      #endregion
+
+      #region Setup & teardown
+
+      [TestInitialize]
+      public void BeforeEach() {
+
+         _builder = new ContainerBuilder();
+         // Does not work
+         //_builder
+         //   .RegisterType(typeof(IRepository<>))
+         //   .AsImplementedInterfaces();
+      }
+
+      [TestCleanup]
+      public void AfterEach() {
+
+         if (_container != null) {
+            _container.Dispose();
+         }
+      }
+
+      #endregion
+
+      #region Tests
+
+      [TestMethod]
+      public void Can_Resolve_Closed_Generic_Types_Explicitly_Registered() {
+
+         // arrange
+         _builder
+            .RegisterType<CustomerRepository>()
+            .AsImplementedInterfaces();
+         _container = _builder.Build();
+
+         // act
+         var instance = _container.Resolve<IRepository<Customer>>();
+
+         // assert
+         Assert.IsInstanceOfType(instance, typeof(CustomerRepository));
+      }
+
+      [TestMethod]
+      public void Can_Resolve_Closed_Generic_Types_Registered_By_Convention() {
+
+         // arrange
+         _builder
+            .RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+            .AsClosedTypesOf(typeof(IRepository<>))
+            .AsImplementedInterfaces();
+         _container = _builder.Build();
+
+         // act
+         var instance = _container.Resolve<IRepository<Customer>>();
+
+         // assert
+         Assert.IsInstanceOfType(instance, typeof(CustomerRepository));
+      }
+
+      [TestMethod]
+      public void Can_Resolve_Closed_Generic_Types_Registered_By_Explicitly_Selecting_Closed_Types() {
+
+         // arrange
+         _builder
+            .RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+            .Where(t => !t.IsAbstract
+                        && t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRepository<>)))
+            .AsImplementedInterfaces();
+         _container = _builder.Build();
+
+         // act
+         var instance = _container.Resolve<IRepository<Customer>>();
+
+         // assert
+         Assert.IsInstanceOfType(instance, typeof(CustomerRepository));
+      }
+
+      #endregion
+   }
+}
---
+++ AutofacSamples/AutofacSamples.Scenarios/Core/ClosedGenerics/Repositories.cs
@@ -0,0 +1,26 @@
+﻿using AutofacSamples.Scenarios.Core.Models;
+
+namespace AutofacSamples.Scenarios.Core.ClosedGenerics {
+   
+   public interface IRepository<out T> {
+
+      #region Apis
+
+      T Get(int id);
+
+      #endregion
+   }
+
+   public class CustomerRepository : IRepository<Customer> {
+
+      #region IRepository
+
+      public Customer Get(int id) {
+         return new Customer() {
+            Id = id
+         };
+      }
+
+      #endregion
+   }
+}
