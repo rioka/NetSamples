adae56647c58a698d305030a5ae6b61f00761aa2
Ricardo Dozzo <Ricardo.Dozzo@adazzle.local>
2016-11-25 16:40:19 +0000

    Samples with values for the constructor set upon registering services

---
+++ AutofacSamples/AutofacSamples.Scenarios/CanSetParameterAtRegistration.cs
@@ -0,0 +1,80 @@
+﻿using System.Reflection;
+using Autofac;
+using AutofacSamples.Scenarios.Core.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutofacSamples.Scenarios {
+   
+   [TestClass]
+   public class CanSetParameterAtRegistration {
+
+      #region Vars
+
+      private ContainerBuilder _builder;
+      private IContainer _container;
+
+      #endregion
+
+      #region Setup & teardown
+
+      [TestInitialize]
+      public void BeforeEach() {
+
+         _builder = new ContainerBuilder();
+         _builder
+            .RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+            .Except<ExtendedService>()
+            .AsImplementedInterfaces();
+      }
+
+      [TestCleanup]
+      public void AfterEach() {
+
+         if (_container != null) {
+            _container.Dispose();
+         }
+      }
+
+      #endregion
+
+      #region Tests
+
+      [TestMethod]
+      public void Can_Register_A_Type_Setting_Values_For_Parameters_By_Type() {
+
+         // arrange
+         _builder
+            .RegisterType<ExtendedService>()
+            .WithParameter(new TypedParameter(typeof(int), 2))
+            .AsImplementedInterfaces();
+         _container = _builder.Build();
+
+         // act
+         var instance = _container.Resolve<IExtendedService>();
+
+         // assert
+         Assert.IsInstanceOfType(instance, typeof(IExtendedService));
+         Assert.AreEqual(2, instance.Retries);
+      }
+
+      [TestMethod]
+      public void Can_Register_A_Type_Setting_Values_For_Parameters_By_Name() {
+
+         // arrange
+         _builder
+            .RegisterType<ExtendedService>()
+            .WithParameter("retries", 2)
+            .AsImplementedInterfaces();
+         _container = _builder.Build();
+
+         // act
+         var instance = _container.Resolve<IExtendedService>();
+
+         // assert
+         Assert.IsInstanceOfType(instance, typeof(IExtendedService));
+         Assert.AreEqual(2, instance.Retries);
+      }
+
+      #endregion
+   }
+}
