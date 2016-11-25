6869cc1a3275bc02c7969dbf0098d4276c86bea2
Ricardo Dozzo <Ricardo.Dozzo@adazzle.local>
2016-11-25 16:52:41 +0000

    Sample code to register and resolve a type via multiple interfaces

---
+++ AutofacSamples/AutofacSamples.Scenarios/CanResolveViaAllRegisteredServices.cs
@@ -0,0 +1,90 @@
+﻿using System.Reflection;
+using Autofac;
+using AutofacSamples.Scenarios.Core.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutofacSamples.Scenarios {
+
+   [TestClass]
+   public class CanResolveViaAllRegisteredServices {
+
+      #region Vars
+
+      private ContainerBuilder _builder;
+      private IContainer _container;
+
+      #endregion
+
+      #region Setup & teardown
+
+      [TestInitialize]
+      public void BeforeEach() {
+
+         _builder = new ContainerBuilder();
+      }
+
+      [TestCleanup]
+      public void AfterEach() {
+
+         if (_container != null) {
+            _container.Dispose();
+         }
+      }
+
+      #endregion
+
+      #region Tests
+
+      [TestMethod]
+      public void Can_Resolve_Via_Base_Type() {
+
+         // arrange
+         _builder
+            .RegisterType<CustomerService>()
+            .As<ServiceBase>();
+         _container = _builder.Build();
+
+         // act
+         var instance = _container.Resolve<ServiceBase>();
+
+         // assert
+         Assert.IsInstanceOfType(instance, typeof(CustomerService));
+      }
+
+      [TestMethod]
+      public void Can_Resolve_Via_Interfaces() {
+
+         // arrange
+         _builder
+            .RegisterType<CustomerService>()
+            .AsImplementedInterfaces()
+            .As<ServiceBase>();
+         _container = _builder.Build();
+
+         // act
+         var instance = _container.Resolve<IUIService>();
+
+         // assert
+         Assert.IsInstanceOfType(instance, typeof(CustomerService));
+      }
+
+      [TestMethod]
+      public void Can_Resolve_Via_Base_Service() {
+
+         // arrange
+         _builder
+            .RegisterType<CustomerService>()
+            .AsImplementedInterfaces()
+            .As<ServiceBase>();
+         _container = _builder.Build();
+
+         // act
+         var instance = _container.Resolve<IServiceBase>();
+
+         // assert
+         Assert.IsInstanceOfType(instance, typeof(CustomerService));
+      }
+
+      #endregion
+   }
+}
---
+++ AutofacSamples/AutofacSamples.Scenarios/Core/Services/CustomerService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace AutofacSamples.Scenarios.Core.Services {
+
+   public interface IServiceBase { }
+
+   public interface IUIService : IServiceBase { }
+
+   public abstract class ServiceBase : IUIService { }
+
+   public class CustomerService : ServiceBase { }
+}
