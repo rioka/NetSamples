59d5c0909c9b367fc0134e2668b37909c849e2e8
rioka <rioka@libero.it>
2017-09-15 12:55:00 +0100

    Settings constraints on expression in Moq

---
+++ MoqSamples/MoqSamples.UseCases/Core/Services/SampleService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace MoqSamples.UseCases.Core.Services {
+
+   public interface IRunner {
+
+      int Run(Expression<Func<int, int>> exp);
+   }
+
+   public class SampleService {
+
+      private readonly IRunner _runner;
+
+      public SampleService(IRunner runner) {
+
+         _runner = runner;
+      }
+
+      public int Do(int i) {
+
+         return _runner.Run(x => x + 2);
+      }
+
+      public int DoOther(int i) {
+
+         return _runner.Run(x => x + 3);
+      }
+   }
+}
---
+++ MoqSamples/MoqSamples.UseCases/SetConstraintOnExpressionParameter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq.Expressions;
+using Moq;
+using MoqSamples.UseCases.Core.Services;
+using NUnit.Framework;
+
+namespace MoqSamples.UseCases {
+
+   [TestFixture]
+   public class SetConstraintOnExpressionParameter {
+
+      [Test]
+      public void Constraint_Works_When_Setup_And_Call_Match() {
+
+         // arrange
+         var runner = new Mock<IRunner>();
+         var done = false;
+         runner
+            .Setup(x => x.Run(It.Is<Expression<Func<int, int>>>(p => CheckAddTwoExp(p))))
+            //.Callback<Expression<Func<int, int>>>(x => done = true);
+            .Returns(5);
+         var service = new SampleService(runner.Object);
+
+         // act
+         var result = service.Do(10);
+
+         // assert
+         //Assert.IsTrue(done);
+         Assert.AreEqual(5, result);
+      }
+
+      [Test]
+      public void Constraint_Works_When_Setup_And_Call_Dont_Match() {
+
+         // arrange
+         var runner = new Mock<IRunner>();
+         runner
+            .Setup(x => x.Run(It.Is<Expression<Func<int, int>>>(p => CheckAddTwoExp(p))))
+            .Returns(5);
+         var service = new SampleService(runner.Object);
+
+         // act
+         var result = service.DoOther(10);
+
+         // assert
+         Assert.AreEqual(0, result);
+      }
+
+      #region Internals
+
+      static bool CheckAddTwoExp(Expression<Func<int, int>> e) {
+
+         var param = e.Parameters[0];
+         var operation = (BinaryExpression) e.Body;
+         var left = (ParameterExpression) operation.Left;
+         var right = (ConstantExpression) operation.Right;
+
+         return operation.NodeType == ExpressionType.Add
+                && (int) right.Value == 2;
+      }
+
+      #endregion
+   }
+}
